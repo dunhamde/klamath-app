b15f60b1f3b7b5b6e995a8175cbf00c002e05016
Derek Dunham <dunham.derek@gmail.com>
2019-10-23 07:05:39 -0700

    header background image

--- src/header/components/header.js
+++ src/header/components/header.js
@@ -1,16 +1,30 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
-import BannerImg from './../../../public/klamathtribes1200.png'
+import BannerImgSource from './../../../public/klamathtribes1200.png'
+import HeaderBackgroundImgSource from '../../../public/headerpic2.jpg'
 
-const SiteHeaderContainer = styled.img`
-  position: fixed;
-  background-color: black;
+const BannerImg = styled.img`
+  // background-color: #d33a2c;
+  position: absolute;
+`
+
+const HeaderBackgroundImg = styled.img`
+  flex: 1;
+`
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: relative;
+  max-width: 100%;
 `
 
 export const SiteHeader = () => {
   return (
     <Fragment>
-      <SiteHeaderContainer src={BannerImg} />
+      <HeaderContainer>
+        <HeaderBackgroundImg src={HeaderBackgroundImgSource} />
+        <BannerImg src={BannerImgSource} />
+      </HeaderContainer>
     </Fragment>
   )
 }
