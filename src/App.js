ddc4c3ada7bb739a52102f2a6af32deffd4b2fbd
Derek Dunham <dunham.derek@gmail.com>
2019-10-22 19:59:41 -0700

    adding eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
-import styled from "styled-components";
-import SiteHeader from "./header/components/header.js";
+import React, { Component } from 'react'
+import { hot } from 'react-hot-loader'
+import styled from 'styled-components'
+import SiteHeader from './header/components/header.js'
 
 const AppContainer = styled.div`
-    margin: 16px;
-    font-family: Arial, Helvetica, sans-serif;
+  margin: 16px;
+  font-family: Arial, Helvetica, sans-serif;
 `
 
-class App extends Component{
-  render(){
-    return(
+class App extends Component {
+  render() {
+    return (
       <AppContainer>
         <SiteHeader />
       </AppContainer>
-    );
+    )
   }
 }
 
-export default hot(module)(App);
+export default hot(module)(App)
--- src/header/components/header.js
+++ src/header/components/header.js
@@ -1,22 +1,18 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import styled from 'styled-components'
 import BannerImg from './../../../public/klamathtribes1200.png'
 
-const SiteHeaderContainer = styled.div`
-    width: 100%;
-    height: 200px;
-    position: fixed;
-    background-color: red;
+const SiteHeaderContainer = styled.img`
+  position: fixed;
+  background-color: black;
 `
 
 export const SiteHeader = () => {
-    return (
-        <Fragment>
-            {/* <SiteHeaderContainer>SiteHeader</SiteHeaderContainer> */}
-            <img src={BannerImg} />
-        </Fragment>
-    )
-
+  return (
+    <Fragment>
+      <SiteHeaderContainer src={BannerImg} />
+    </Fragment>
+  )
 }
 
 export default SiteHeader
