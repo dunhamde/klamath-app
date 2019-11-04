ee7f3ca6f8ae9b4d6a3a8be08a91e6209bcdcf99
Derek Dunham <dunham.derek@gmail.com>
2019-11-04 12:56:17 -0800

    improvement(top header): adding link animations and updating styling

--- src/components/top-header/top-header.js
+++ src/components/top-header/top-header.js
@@ -5,20 +5,44 @@ import TopHeaderSocialLinksList from './th-social-links-list.js'
 
 const HeaderContainer = styled.div`
   display: flex;
-  justify-content: space-around;
+  justify-content: space-between;
   background-image: linear-gradient(rgb(0, 0, 255), rgb(0, 128, 0));
   max-width: 100%;
-  background-color: green;
   height: 48px;
   ul {
     display: flex;
   }
   li {
-    background-color: orange;
     list-style: none;
-    border: 1px solid black;
-    padding: 0 8px;
-    margin: 0 4px;
+    margin: 0 12px;
+  }
+  a {
+    font-family: 'Source Sans Pro', Arial, sans-serif;
+    color: white;
+    font-size: 16.5px;
+    font-weight: 400;
+    letter-spacing: 1px;
+    line-height: 24px;
+    text-shadow: black 2px 2px 0;
+  }
+  a:hover {
+    color: rgba(221, 133, 0, 0.7);
+  }
+  li::after {
+    margin-top: 2px;
+    content: '';
+    width: 100%;
+    height: 4px;
+    background: rgba(221, 133, 0, 0.7);
+    display: block;
+    transform: rotateY(90deg);
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+  }
+  li:hover::after {
+    transform: rotateY(180deg);
   }
 `
 
