f5262bd2675b975166679a99278bcbf5d60c2efb
Derek Dunham <dunham.derek@gmail.com>
2019-11-06 08:41:36 -0800

    improvement(carousel): background image as prop

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,17 +1,27 @@
 import React from 'react'
 import styled from 'styled-components'
 import FishRestoGoalImgSrc from '../../../public/carousel-images/slide1-1800x500.jpg'
+import TribalCouncilImgSrc from '../../../public/carousel-images/Tribal-Council-East-Good_Slider-1422x500.jpg'
 
 export const CarouselContainer = styled.div`
   max-width: 100%;
   height: 504px;
-  border: 6px solid turquoise;
-  background-image: url(${FishRestoGoalImgSrc});
+`
+
+export const CarouselFrame = styled.div`
+  max-width: 100%;
+  height: 100%;
+  border: 4px solid red;
   background-size: cover;
+  background-image: url(${props => props.backgroundImage});
 `
 
 export const Carousel = () => {
-  return <CarouselContainer>Content carousel</CarouselContainer>
+  return (
+    <CarouselContainer>
+      <CarouselFrame backgroundImage={FishRestoGoalImgSrc} />
+    </CarouselContainer>
+  )
 }
 
 export default Carousel
