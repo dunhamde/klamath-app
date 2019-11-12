9fbde61863315cc7a19c9189cdac23a6f221f531
Derek Dunham <dunham.derek@gmail.com>
2019-11-12 13:12:04 -0800

    improvement(carousel): more images

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -2,12 +2,19 @@ import React from 'react'
 import styled from 'styled-components'
 import FishRestoGoalImgSrc from '../../../public/carousel-images/slide1-1800x500.jpg'
 import TribalCouncilImgSrc from '../../../public/carousel-images/Tribal-Council-East-Good_Slider-1422x500.jpg'
+import LowWaterLevelImgSrc from '../../../public/carousel-images/slime-edge-1712x500.jpg'
+import AlgaeImgSrc from '../../../public/carousel-images/Ron_Larson_AFA_3S-1769x500.jpg'
+import FisheryCollapseImgSrc from '../../../public/carousel-images/deadfish-1700x500.jpg'
+import SolutionImgSrc from '../../../public/carousel-images/yuck-1-1712x500.jpg'
+import CwaamImgSrc from '../../../public/carousel-images/cwaamdark-1500x500.jpg'
 
 export const CarouselContainer = styled.div`
   display: flex;
   max-width: 100%;
   height: 504px;
 `
+export const CarouselTrack = styled.div``
+export const CarouselControls = styled.div``
 
 export const CarouselSlide = styled.div`
   width: 100%;
@@ -18,8 +25,6 @@ export const CarouselSlide = styled.div`
 
 export const ControlRadio = styled.input``
 export const ControlLabel = styled.label``
-export const CarouselTrack = styled.div``
-export const CarouselControls = styled.div``
 export const CarouselIndicators = styled.div``
 
 export const Carousel = () => (
@@ -44,6 +49,9 @@ export const Carousel = () => (
       />
     </CarouselControls>
     <CarouselTrack>
+      <CarouselSlide backgroundImage={FishRestoGoalImgSrc} />
+      <CarouselSlide backgroundImage={TribalCouncilImgSrc} />
+      <CarouselSlide backgroundImage={FishRestoGoalImgSrc} />
       <CarouselSlide backgroundImage={FishRestoGoalImgSrc} />
       <CarouselSlide backgroundImage={TribalCouncilImgSrc} />
     </CarouselTrack>
