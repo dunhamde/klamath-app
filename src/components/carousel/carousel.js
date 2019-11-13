425d6d7dd03f54ac89619b0ad47c41ae0d74ef71
Derek Dunham <dunham.derek@gmail.com>
2019-11-13 15:57:59 -0800

    improvement(carousel): forward and backward nav and edited images for scale

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,68 +1,91 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import FishRestoGoalImgSrc from '../../../public/carousel-images/slide1-1800x500.jpg'
+import FishRestoGoalImgSrc from '../../../public/carousel-images/edited-slide1.jpg'
 import TribalCouncilImgSrc from '../../../public/carousel-images/Tribal-Council-East-Good_Slider-1422x500.jpg'
-import LowWaterLevelImgSrc from '../../../public/carousel-images/slime-edge-1712x500.jpg'
-import AlgaeImgSrc from '../../../public/carousel-images/Ron_Larson_AFA_3S-1769x500.jpg'
-import FisheryCollapseImgSrc from '../../../public/carousel-images/deadfish-1700x500.jpg'
-import SolutionImgSrc from '../../../public/carousel-images/yuck-1-1712x500.jpg'
-import CwaamImgSrc from '../../../public/carousel-images/cwaamdark-1500x500.jpg'
+import LowWaterLevelImgSrc from '../../../public/carousel-images/edited-slime-edge.jpg'
+import AlgaeImgSrc from '../../../public/carousel-images/edited-Ron_Larson_AFA_3S.jpg'
+import FisheryCollapseImgSrc from '../../../public/carousel-images/edited-deadfish.jpg'
+import SolutionImgSrc from '../../../public/carousel-images/edited-yuck-1.jpg'
+import CwaamImgSrc from '../../../public/carousel-images/edited-cwaamdark.jpg'
 
 export const CarouselContainer = styled.div`
-  height: 504px;
+  display: flex;
+  position: relative;
+  max-width: 100%;
+  align-items: center;
+  justify-content: center;
 `
-export const CarouselTrack = styled.div``
 
-export const CarouselControls = styled.div``
+export const CarouselControls = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`
 
 export const CarouselImage = styled.img`
   width: 100%;
-  height: auto;
-  object-fit: contain;
-  position: absolute;
-  transform: translateX(calc(${props => props.order - 1} * 100%));
 `
 
-export const ControlRadio = styled.input``
-export const ControlLabel = styled.label``
+export const ControlLabel = styled.div`
+  width: 36px;
+  height: 36px;
+  z-index: 2;
+  transform: ${props =>
+    props.backward ? 'rotate(-135deg)' : 'rotate(45deg)'};
+  margin: 24px;
+  border-width: 5px 5px 0 0;
+  border-style: solid;
+  border-color: #fafafa;
+  opacity: 0.5;
+  outline: 0;
+  cursor: pointer;
+  :hover {
+    opacity: 1;
+  }
+`
 export const CarouselIndicators = styled.div``
 
-export const Carousel = () => (
-  <CarouselContainer>
-    {/* <CarouselControls>
-      <ControlRadio
-        type="radio"
-        id="1"
-        name="activator"
-        checked="checked"
-      />
+export const Carousel = () => {
+  const [activeImage, setActiveImage] = useState(0)
+  const carouselImages = [
+    FishRestoGoalImgSrc,
+    TribalCouncilImgSrc,
+    LowWaterLevelImgSrc,
+    AlgaeImgSrc,
+    FisheryCollapseImgSrc,
+    SolutionImgSrc,
+    CwaamImgSrc,
+  ]
+  return (
+    <CarouselContainer>
+      <CarouselControls>
+        <ControlLabel
+          backward
+          onClick={() =>
+            setActiveImage(activeImage - 1 >= 0 ? activeImage - 1 : 0)
+          }
+        />
+        <ControlLabel
+          forward
+          onClick={() =>
+            setActiveImage(
+              activeImage + 1 <= carouselImages.length - 1
+                ? activeImage + 1
+                : carouselImages.length - 1,
+            )
+          }
+        />
+      </CarouselControls>
+      <CarouselImage src={carouselImages[activeImage]} />
 
-      <ControlRadio type="radio" id="2" name="activator" />
-      <ControlRadio type="radio" id="3" name="activator" />
-      <ControlLabel
-        htmlFor="1"
-        className="carousel__control carousel__control--forward"
-      />
-      <ControlLabel
-        htmlFor="2"
-        className="carousel__control carousel__control--backward"
-      />
-    </CarouselControls> */}
-    <CarouselTrack>
-      <CarouselImage src={FishRestoGoalImgSrc} order="1" />
-      <CarouselImage src={TribalCouncilImgSrc} order="2" />
-      <CarouselImage src={LowWaterLevelImgSrc} order="3" />
-      <CarouselImage src={AlgaeImgSrc} order="4" />
-      <CarouselImage src={FisheryCollapseImgSrc} order="5" />
-      <CarouselImage src={SolutionImgSrc} order="6" />
-      <CarouselImage src={CwaamImgSrc} order="7" />
-    </CarouselTrack>
-    {/* <CarouselIndicators>
+      {/* <CarouselIndicators>
       <ControlLabel htmlFor="1" className="carousel__indicator" />
       <ControlLabel htmlFor="2" className="carousel__indicator" />
       <ControlLabel htmlFor="3" className="carousel__indicator" />
     </CarouselIndicators> */}
-  </CarouselContainer>
-)
+    </CarouselContainer>
+  )
+}
 
 export default Carousel
