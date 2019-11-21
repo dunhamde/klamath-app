8742d8699bded16e64225cd3e85c63b0857dc910
Derek Dunham <dunham.derek@gmail.com>
2019-11-21 10:28:02 -0800

    improvement(header): add burger menu styling boilerplate

--- src/components/floating-header/floating-header.js
+++ src/components/floating-header/floating-header.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
+import { slide as Menu } from 'react-burger-menu'
 import HeaderLogoSource from '../../../public/klamathtribes1200.png'
 import HeaderBannerSource from '../../../public/headerpic2.jpg'
 import FloatingHeaderLinksList from './fh-links-list'
-import { slide as Menu } from 'react-burger-menu'
 
 const HeaderLogo = styled.img`
   align-self: flex-start;
@@ -79,12 +79,71 @@ const HeaderContainer = styled.div`
   }
 `
 
+export const StyledBurgerMenu = styled.div`
+  /* Position and sizing of burger button */
+  .bm-burger-button {
+    position: fixed;
+    width: 36px;
+    height: 30px;
+    left: 36px;
+    top: 36px;
+  }
+
+  /* Color/shape of burger icon bars */
+  .bm-burger-bars {
+    background: #373a47;
+  }
+
+  /* Position and sizing of clickable cross button */
+  .bm-cross-button {
+    height: 24px;
+    width: 24px;
+  }
+
+  /* Color/shape of close button cross */
+  .bm-cross {
+    background: #bdc3c7;
+  }
+
+  /* General sidebar styles */
+  .bm-menu {
+    background: #373a47;
+    padding: 2.5em 1.5em 0;
+    font-size: 1.15em;
+  }
+
+  /* Morph shape necessary with bubble or elastic */
+  .bm-morph-shape {
+    fill: #373a47;
+  }
+
+  /* Wrapper for item list */
+  .bm-item-list {
+    color: #b8b7ad;
+    padding: 0.8em;
+  }
+
+  /* Individual item */
+  .bm-item {
+    display: inline-block;
+  }
+
+  /* Styling of overlay */
+  .bm-overlay {
+    background: rgba(0, 0, 0, 0.3);
+  }  }
+`
+
 export const TopHeader = () => {
   return (
     <HeaderContainer>
       <HeaderLogo src={HeaderLogoSource} />
       <FloatingHeaderLinksList />
-      <BurgerMenu />
+      <StyledBurgerMenu>
+        <Menu>
+          <FloatingHeaderLinksList />
+        </Menu>
+      </StyledBurgerMenu>
     </HeaderContainer>
   )
 }
