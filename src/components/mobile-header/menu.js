bf2cd3c4fabd1f2c214ab2a85710ed01990f5261
Derek Dunham <dunham.derek@gmail.com>
2019-11-27 14:02:17 -0800

    improvement(header): update onclick styling for burger menu

--- src/components/mobile-header/menu.js
+++ src/components/mobile-header/menu.js
@@ -8,12 +8,36 @@ export const HamburgerMenu = styled.div`
     background-color: rgba(221, 133, 0, 0.7);
     margin: 6px 0;
   }
+
+  /* Rotate the first bar */
+  div:first-child {
+    -webkit-transform: ${props =>
+      props.menuOpen && 'rotate(-45deg) translate(-9px, 6px)'};
+    transform: ${props =>
+      props.menuOpen && 'rotate(-45deg) translate(-9px, 6px)'};
+  }
+
+  /* Fade out the second bar */
+  div:nth-child(2) {
+    opacity: ${props => props.menuOpen && 0};
+  }
+
+  /* Rotate last bar */
+  div:nth-child(3) {
+    -webkit-transform: ${props =>
+      props.menuOpen && 'rotate(45deg) translate(-8px, -8px)'};
+    transform: ${props =>
+      props.menuOpen && 'rotate(45deg) translate(-8px, -8px)'};
+  }
 `
 
 export const Menu = () => {
-  const [menuOpen, setMenuState] = useState(0)
+  const [menuOpen, setMenuState] = useState(false)
   return (
-    <HamburgerMenu>
+    <HamburgerMenu
+      onClick={() => setMenuState(!menuOpen)}
+      menuOpen={menuOpen}
+    >
       <div />
       <div />
       <div />
